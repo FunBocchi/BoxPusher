f4370375f0944ef3c4df47be552ade85eee253a5
子翔 林 <13735999188@163.com>
2023-10-26 15:23:48 +0800

    增加了读取文件的功能

--- ConsApp01/Apprun.cpp
+++ ConsApp01/Apprun.cpp
@@ -1,6 +1,23 @@
 #include "Classdefault.h"
-
+#include "FileRead.h"
 int main() {
+	std::ifstream inputFile("stageData.txt", std::ifstream::binary);
+	inputFile.seekg(0, std::ifstream::end);
+
+	int fileSize = static_cast<int>(inputFile.tellg());
+	inputFile.seekg(0, std::ifstream::beg);
+	char* fileImage = new char[fileSize];
+	inputFile.read(fileImage, fileSize);
+
+	std::cout.write(fileImage, fileSize);
+	std::cout << std::endl;
+
+
+
+
+
+
+
 	Object* mapInfo = new Object[lom * wom];
 	InitMap(mapInfo, defaultMapData);
 	DrawMap(mapInfo);
